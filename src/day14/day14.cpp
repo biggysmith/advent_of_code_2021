cf7359969d91df4c2f1dd6f3be0aa09d72f6bf14
Scott Smith <ssmith@tmvse.com>
2021-12-15 00:29:52 +0000

    day14

---
+++ src/day14/day14.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <list>
+#include <map>
+
+struct instructions_t{
+    std::string polymer_template;
+    std::map<std::string,char> instructions;
+};
+
+instructions_t load_input(const std::string& file){
+    instructions_t ret;
+    std::ifstream fs(file);
+    std::string line;
+
+    std::getline(fs, line);
+    ret.polymer_template = line;
+
+    std::getline(fs, line);
+    while (std::getline(fs, line)) {
+        ret.instructions[line.substr(0,2)] = line[6];
+    }
+    return ret;
+}
+
+size_t process(const instructions_t& data, int steps)
+{
+    std::map<std::string,size_t> combos;
+
+    for(int i=0; i<data.polymer_template.size()-1; ++i){
+        combos[data.polymer_template.substr(i,2)]++;
+    }
+
+    for(int step=0; step<steps; ++step){
+        std::map<std::string,size_t> new_combos;
+        for(auto& combo : combos){
+            if(combo.second){
+                new_combos[combo.first.substr(0,1) + data.instructions.at(combo.first)] += combo.second;
+                new_combos[data.instructions.at(combo.first) + combo.first.substr(1,2)] += combo.second;
+            } 
+        }
+        combos = new_combos;
+    }
+
+    std::map<char,size_t> hist;
+    for(auto& elements : combos){
+        hist[elements.first[0]] += elements.second;
+        hist[elements.first[1]] += elements.second;
+    }
+
+    auto round_up = [](size_t a,size_t b){
+        return (a+b-1)/b;
+    };
+
+    auto [mn, mx] = std::minmax_element(hist.begin(), hist.end(), [] (auto& p1, auto& p2) {
+        return p1.second < p2.second;
+    });
+
+    return round_up(mx->second,2) - round_up(mn->second,2);
+}
+
+size_t part1(const instructions_t& data)
+{
+    return process(data, 10);
+}
+
+size_t part2(const instructions_t& data)
+{
+    return process(data, 40);
+}
+
+void main()
+{
+    auto test_values = load_input("../src/day14/example_input.txt");
+    auto actual_values = load_input("../src/day14/input.txt");
+
+    std::cout << "part1: " << part1(test_values) << std::endl;
+    std::cout << "part1: " << part1(actual_values) << std::endl;
+
+    std::cout << "part2: " << part2(test_values) << std::endl;
+    std::cout << "part2: " << part2(actual_values) << std::endl;
+}
