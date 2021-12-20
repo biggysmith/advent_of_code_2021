f4fe4463181023648c0c924ed019e8c7f6a3ee46
Scott Smith <ssmith@tmvse.com>
2021-12-20 05:36:01 +0000

    day19

--- src/day19/day19.cpp
+++ src/day19/day19.cpp
@@ -1,244 +1,3 @@
-#if 0
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <numeric>
-#include <set>
-#include <map>
-#include <optional>
-#include <functional>
-#include <iterator>
-
-struct vec3{
-    int x,y,z;
-};
-
-bool operator==(const vec3& a,const vec3& b){
-    return a.x==b.x && a.y==b.y && a.z==b.z;
-}
-
-int cos2(int x){
-	if (x == 90)
-		return 0;
-	if (x == 180)
-		return -1;
-	if (x == 270)
-		return 0;
-	return 1;
-}
-
-int sin2(int x){
-	if (x == 90)
-		return 1;
-	if (x == 180)
-		return 0;
-	if (x == 270)
-		return -1;
-	return 0;
-}
-
-vec3 rotate3d_z(vec3 v, int theta){
-	int c = cos2(theta);
-    int s = sin2(theta);
-    return { v.x*c - v.y*s, v.x*s+v.y*c, v.z };
-}
-
-vec3 rotate3d_x(vec3 v, int theta){
-	int c = cos2(theta);
-    int s = sin2(theta);
-    return {v.x, v.y*c - v.z*s, v.y*s + v.z*c};
-}
-
-vec3 rotate3d_y(vec3 v, int theta){
-	int c = cos2(theta);
-    int s = sin2(theta);
-    return {v.x*c + v.z*s, v.y, -v.x*s + v.z*c};
-}
-
-std::function<vec3(vec3)> apply(int rx, int ry, int rz){
-	auto f = [=](vec3 v){
-		v = rotate3d_x(v, rx);
-		v = rotate3d_y(v, ry);
-		return rotate3d_z(v, rz);
-    };
-	return f;
-}
-
-std::vector<std::function<vec3(vec3)>> rotations = {
-	apply(  0,   0,   0),
-	apply( 90,   0,   0),
-	apply(180,   0,   0),
-	apply(270,   0,   0),
-	apply(  0,  90,   0),
-	apply( 90,  90,   0),
-	apply(180,  90,   0),
-	apply(270,  90,   0),
-	apply(  0, 180,   0),
-	apply( 90, 180,   0),
-	apply(180, 180,   0),
-	apply(270, 180,   0),
-	apply(  0, 270,   0),
-	apply( 90, 270,   0),
-	apply(180, 270,   0),
-	apply(270, 270,   0),
-	apply(  0,   0,  90),
-	apply( 90,   0,  90),
-	apply(180,   0,  90),
-	apply(270,   0,  90),
-	apply(  0,   0, 270),
-	apply( 90,   0, 270),
-	apply(180,   0, 270),
-	apply(270,   0, 270),
-};
-
-
-
-
-std::vector<std::vector<vec3>> load_input(const std::string& file){
-    std::vector<std::vector<vec3>> ret;
-    std::ifstream fs(file);
-    std::string line;
-    while (std::getline(fs, line)) {
-        if(line.find("---") != std::string::npos){
-            ret.push_back(std::vector<vec3>());
-            continue;
-        }
-        if(line.empty()){
-            continue;
-        }
-        char comma = ',';
-        int x,y,z;
-        std::stringstream ss(line);
-        ss >> x >> comma >> y >> comma >> z;
-        ret.back().push_back({ x, y, z });
-    }
-    return ret;
-}
-
-vec3 basis_transform(const vec3& v, const std::vector<vec3>& basis){
-    return { basis[0].x*v.x+basis[0].y*v.y+basis[0].z*v.z, basis[1].x*v.x+basis[1].y*v.y+basis[1].z*v.z, basis[2].x*v.x+basis[2].y*v.y+basis[2].z*v.z };
-}
-
-vec3 diff(const vec3& a, const vec3& b){
-    return { a.x - b.x, a.y - b.y, a.z - b.z };
-}
-
-vec3 add(const vec3& a, const vec3& b){
-    return { a.x + b.x, a.y + b.y, a.z + b.z };
-}
-
-std::vector<std::vector<vec3>> bases;
-
-std::optional<std::pair<std::vector<vec3>,vec3>> common_points(const std::vector<vec3>& s1,const std::vector<vec3>& s2)
-{
-    for(auto& basis : bases){
-		//new_s2 = tuple(basis_transform(vec, basis) for vec in s2)
-        std::vector<vec3> new_s2(s2.size());
-        for(int i=0; i<new_s2.size(); ++i){
-            new_s2[i] = basis_transform(s2[i], basis);
-        }
-
-        for(int i=0; i<s1.size(); ++i){
-            for(int j=0; j<new_s2.size(); ++j){
-                vec3 dist = diff(s1[i], new_s2[j]);
-
-                std::vector<vec3> translated_s2(new_s2.size());
-                for(int k=0; k<translated_s2.size(); ++k){
-                    translated_s2[i] = add(new_s2[k], dist);
-                }
-
-                std::vector<vec3> inter;
-                for(auto& a : s1){
-                    for(auto& b : translated_s2){
-                        if(a == b){
-                            inter.push_back(a);
-                        }
-                    }
-                }
-
-                if(inter.size() >= 12){
-                    return std::make_pair(translated_s2,dist);
-                }
-            }
-        }
-    }
-
-    return std::nullopt;
-}
-
-int part1(const std::vector<std::vector<vec3>>& scanners)
-{
-    std::vector<vec3> identity{
-        {1,0,0},
-        {0,1,0},
-        {0,0,1}
-    };
-
-    //std::vector<std::vector<vec3>> bases;
-    for(auto& f : rotations){
-        bases.push_back(std::vector<vec3>{ f(identity[0]), f(identity[1]), f(identity[1]) });
-    }
-
-    std::map<int,std::vector<vec3>> matched;
-    matched[0] = scanners[0];
-    std::vector<vec3> distances = {{0,0,0}};
-    std::set<int> done;
-
-    while(true)
-    {
-	    for(int i=0; i<scanners.size(); ++i){
-		    if (done.count(i) || !matched.count(i)){
-			    continue;
-            }
-
-            for(int j=0; j<scanners.size(); ++j){
-		        if (matched.count(j) || i == j){
-			        continue;
-                }
-
-			    auto cp = common_points(matched[i], scanners[j]);
-
-			    if(cp){
-				    matched[j] = cp->first;
-				    distances.push_back(cp->second);
-                }
-            }
-
-		    done.insert(i);
-        }
-
-	    if (done.size() == scanners.size()){
-		    break;
-        }
-    };
-
-    return 0;
-}
-
-int part2(const std::vector<std::vector<vec3>>& numbers)
-{
-    return 0;
-}
-
-void main()
-{
-    auto test_values = load_input("../src/day19/example_input.txt");
-    auto actual_values = load_input("../src/day19/input.txt");
-
-    std::cout << "part1: " << part1(test_values) << std::endl;
-    //std::cout << "part1: " << part1(actual_values) << std::endl;
-
-    /*std::cout << "part2: " << part2(test_values) << std::endl;
-    std::cout << "part2: " << part2(actual_values) << std::endl;*/
-}
-#endif
-
-
-
-#if 1
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -260,151 +19,47 @@ struct matrix33{
     vec3 r0,r1,r2;
 };
 
-int cos2(int x){
-	if (x == 90)
-		return 0;
-	if (x == 180)
-		return -1;
-	if (x == 270)
-		return 0;
-	return 1;
-}
-
-int sin2(int x){
-	if (x == 90)
-		return 1;
-	if (x == 180)
-		return 0;
-	if (x == 270)
-		return -1;
-	return 0;
-}
+int sin_dir[] = { 0, 1, 0, -1 };
+int cos_dir[] = { 1, 0, -1, 0 };
 
 template <int axis> 
 matrix33 rotation_axis(const int v){
-    int s = sin2(v);
-    int c = cos2(v);
-    if(axis == 0){
-        vec3 r0{1,0,0};
-        vec3 r1{0,c,-s};
-        vec3 r2{0,s,c};
-        return matrix33{r0,r1,r2};
-    }else if(axis == 1){
-        vec3 r0{c,0,s};
-        vec3 r1{0,1,0};
-        vec3 r2{-s,0,c};
-        return matrix33{r0,r1,r2};
+    int s = sin_dir[v];
+    int c = cos_dir[v];
+    if constexpr(axis == 0){
+        return {{1,0,0}, {0,c,-s}, {0,s,c}};
+    }else if constexpr(axis == 1){
+        return {{c,0,s}, {0,1,0}, {-s,0,c}};
     }else{
-        vec3 r0{c,-s,0};
-        vec3 r1{s,c,0};
-        vec3 r2{0,0,1};
-        return matrix33{r0,r1,r2};
+        return {{c,-s,0}, {s,c,0}, {0,0,1}};
     }
-}
-
-vec3 rotate3d_z(vec3 v, int theta){
-	int c = cos2(theta);
-    int s = sin2(theta);
-    return { v.x*c - v.y*s, v.x*s+v.y*c, v.z };
-}
-
-vec3 rotate3d_x(vec3 v, int theta){
-	int c = cos2(theta);
-    int s = sin2(theta);
-    return {v.x, v.y*c - v.z*s, v.y*s + v.z*c};
-}
-
-vec3 rotate3d_y(vec3 v, int theta){
-	int c = cos2(theta);
-    int s = sin2(theta);
-    return {v.x*c + v.z*s, v.y, -v.x*s + v.z*c};
-}
-
-std::function<vec3(vec3)> apply(int rx, int ry, int rz){
-	auto f = [=](vec3 v){
-		v = rotate3d_x(v, rx);
-		v = rotate3d_y(v, ry);
-		return rotate3d_z(v, rz);
-    };
-	return f;
 }
 
 int dot(const vec3& a,const vec3& b){ return a.x*b.x + a.y*b.y + a.z*b.z; }
 
+vec3 operator*(const matrix33& a,const vec3& b) { return { dot(a.r0,b), dot(a.r1,b), dot(a.r2,b) }; }
+
+vec3 operator-(const vec3& a,const vec3& b) { return { a.x-b.x, a.y-b.y, a.z-b.z }; }
+
+vec3 operator+(const vec3& a,const vec3& b) { return { a.x+b.x, a.y+b.y, a.z+b.z }; }
+
+bool operator==(const vec3& a,const vec3& b) { return a.x==b.x && a.y==b.y && a.z==b.z; }
+
+bool operator==(const matrix33& a,const matrix33& b) { return a.r0==b.r0 && a.r1==b.r1 && a.r2==b.r2; }
+
+bool operator<(const vec3& a,const vec3& b) { return std::make_tuple(a.x,a.y,a.z) < std::make_tuple(b.x,b.y,b.z); }
+
 matrix33 operator*(const matrix33& a,const matrix33& b) {
-    return matrix33{
-        vec3{ dot(a.r0,{ b.r0.x, b.r1.x, b.r2.x }), dot(a.r0,{ b.r0.y, b.r1.y, b.r2.y }), dot(a.r0,{ b.r0.z, b.r1.z, b.r2.z }) },
-        vec3{ dot(a.r1,{ b.r0.x, b.r1.x, b.r2.x }), dot(a.r1,{ b.r0.y, b.r1.y, b.r2.y }), dot(a.r1,{ b.r0.z, b.r1.z, b.r2.z }) },
-        vec3{ dot(a.r2,{ b.r0.x, b.r1.x, b.r2.x }), dot(a.r2,{ b.r0.y, b.r1.y, b.r2.y }), dot(a.r2,{ b.r0.z, b.r1.z, b.r2.z }) }
+    return {
+        { dot(a.r0,{ b.r0.x, b.r1.x, b.r2.x }), dot(a.r0,{ b.r0.y, b.r1.y, b.r2.y }), dot(a.r0,{ b.r0.z, b.r1.z, b.r2.z }) },
+        { dot(a.r1,{ b.r0.x, b.r1.x, b.r2.x }), dot(a.r1,{ b.r0.y, b.r1.y, b.r2.y }), dot(a.r1,{ b.r0.z, b.r1.z, b.r2.z }) },
+        { dot(a.r2,{ b.r0.x, b.r1.x, b.r2.x }), dot(a.r2,{ b.r0.y, b.r1.y, b.r2.y }), dot(a.r2,{ b.r0.z, b.r1.z, b.r2.z }) }
     };
-}
-
-vec3 operator*(const matrix33& a,const vec3& b) {
-    return { dot(a.r0,b), dot(a.r1,b), dot(a.r2,b) };
-}
-
-vec3 operator-(const vec3& a,const vec3& b) {
-    return { a.x-b.x, a.y-b.y, a.z-b.z };
-}
-
-vec3 operator+(const vec3& a,const vec3& b) {
-    return { a.x+b.x, a.y+b.y, a.z+b.z };
-}
-
-bool operator==(const vec3& a,const vec3& b) {
-    return a.x==b.x && a.y==b.y && a.z==b.z;
-}
-
-bool operator==(const matrix33& a,const matrix33& b) {
-    return a.r0==b.r0 && a.r1==b.r1 && a.r2==b.r2;
-}
-
-bool operator<(const vec3& a,const vec3& b) {
-    return std::make_tuple(a.x,a.y,a.z) < std::make_tuple(b.x,b.y,b.z);
 }
 
 matrix33 rotation_matrix(int rx,int ry,int rz){
     return rotation_axis<2>(rz) * rotation_axis<1>(ry) * rotation_axis<0>(rx);
 }
-
-std::ostream& operator<<(std::ostream& os, const vec3& v){
-    os << v.x << ", " << v.y << ", " << v.z;
-    return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const matrix33& v){
-    os << v.r0 << std::endl;
-    os << v.r1 << std::endl;
-    os << v.r2;
-    return os;
-}
-
-std::vector<std::function<vec3(vec3)>> rotations = {
-	apply(  0,   0,   0),
-	apply( 90,   0,   0),
-	apply(180,   0,   0),
-	apply(270,   0,   0),
-	apply(  0,  90,   0),
-	apply( 90,  90,   0),
-	apply(180,  90,   0),
-	apply(270,  90,   0),
-	apply(  0, 180,   0),
-	apply( 90, 180,   0),
-	apply(180, 180,   0),
-	apply(270, 180,   0),
-	apply(  0, 270,   0),
-	apply( 90, 270,   0),
-	apply(180, 270,   0),
-	apply(270, 270,   0),
-	apply(  0,   0,  90),
-	apply( 90,   0,  90),
-	apply(180,   0,  90),
-	apply(270,   0,  90),
-	apply(  0,   0, 270),
-	apply( 90,   0, 270),
-	apply(180,   0, 270),
-	apply(270,   0, 270),
-};
 
 using set_t = std::set<vec3>;
 
@@ -429,39 +84,36 @@ std::vector<set_t> load_input(const std::string& file){
     return ret;
 }
 
-std::vector<matrix33> bases;
-
-
-
-std::optional<std::pair<set_t,vec3>> common_points(const set_t& s1,const set_t& s2)
+auto overlapping_points(const std::vector<matrix33>& rotations, const set_t& beacons1, const set_t& beacons2)
 {
-    for(auto& rot : bases){
-        set_t new_s2;
-        for(auto& v : s2){
-            new_s2.insert(rot * v);
+    for(auto& rot : rotations)
+    {
+        set_t rotated_beacons2;
+        for(auto& beacon : beacons2){
+            rotated_beacons2.insert(rot * beacon);
         }
 
-        for(auto& v0 : s1){
-            for(auto& v1 : new_s2){
-                vec3 dist = v0 - v1;
+        for(auto& b0 : beacons1){
+            for(auto& b1 : rotated_beacons2){
+                vec3 dist = b0 - b1;
 
-                set_t translated_s2;
-                for(auto& v : new_s2){
-                    translated_s2.insert(v + dist);
+                set_t shifted_beacons2;
+                for(auto& b2 : rotated_beacons2){
+                    shifted_beacons2.insert(b2 + dist);
                 }
 
                 set_t inter;
-                std::set_intersection(s1.begin(), s1.end(), translated_s2.begin(), translated_s2.end(), std::inserter(inter,inter.end()));
+                std::set_intersection(beacons1.begin(), beacons1.end(), shifted_beacons2.begin(), shifted_beacons2.end(), std::inserter(inter,inter.end()));
 
                 if(inter.size() >= 12){
-                    return std::make_pair(translated_s2,dist);
+                    return std::make_pair(shifted_beacons2, dist);
                 }
 
             }
         }
     }
 
-    return std::nullopt;
+    return std::make_pair(set_t(),vec3());
 }
 
 int manhatten(const vec3& a,const vec3& b){
@@ -470,62 +122,58 @@ int manhatten(const vec3& a,const vec3& b){
 
 auto process(const std::vector<set_t>& scanners)
 {
-    matrix33 identity{
-        {1,0,0},
-        {0,1,0},
-        {0,0,1}
+    std::vector<matrix33> rotations = { 
+        rotation_matrix(0,0,0), rotation_matrix(1,0,0), rotation_matrix(2,0,0), rotation_matrix(3,0,0), 
+        rotation_matrix(0,1,0), rotation_matrix(1,1,0), rotation_matrix(2,1,0), rotation_matrix(3,1,0),
+        rotation_matrix(0,2,0), rotation_matrix(1,2,0), rotation_matrix(2,2,0), rotation_matrix(3,2,0),
+        rotation_matrix(0,3,0), rotation_matrix(1,3,0), rotation_matrix(2,3,0), rotation_matrix(3,3,0),
+        rotation_matrix(0,0,1), rotation_matrix(1,0,1), rotation_matrix(2,0,1), rotation_matrix(3,0,1),
+        rotation_matrix(0,0,3), rotation_matrix(1,0,3), rotation_matrix(2,0,3), rotation_matrix(3,0,3),
     };
-    for(auto& f : rotations){
-        bases.push_back(matrix33{ f(identity.r0), f(identity.r1), f(identity.r2) });
-    }
 
-    std::map<int,set_t> matched;
-    matched[0] = scanners[0];
+    std::map<int,set_t> found;
+    found[0] = scanners[0];
 
-    std::vector<vec3> distances;
-    distances.push_back({0,0,0});
-    std::set<int> done;
+    std::vector<vec3> dists;
+    dists.push_back({0,0,0});
 
-    while(true)
+    uint64_t done = 0;
+    uint64_t found_mask = 1;
+
+    while(dists.size() != scanners.size())
     {
         for(int i=0; i<scanners.size(); ++i){
-            if(done.count(i) || !matched.count(i)){
+            if((done & (1ull << i)) || !(found_mask & (1ull << i))){
                 continue;
             }
         
             for(int j=0; j<scanners.size(); ++j){
-                if(matched.count(j) || i == j){
+                if((found_mask & (1ull << j)) || i == j){
                     continue;
                 }
 
-                auto cp = common_points(matched[i], scanners[j]);
+                auto points = overlapping_points(rotations, found[i], scanners[j]);
 
-                if(cp){
-                    matched[j] = cp->first;
-                    //matched.insert(matched.end(), cp->first.begin(), cp->first.end());
-                    distances.push_back(cp->second);
+                if(!points.first.empty()){
+                    found_mask |= 1ull << j;
+                    found[j] = points.first;
+                    dists.push_back(points.second);
                 }
             }
-        
-            done.insert(i);
-        }
 
-        if(done.size() == scanners.size()){
-            break;
+            done |= 1ull << i;
         }
-    
     }
 
-
     set_t points;
-    for(auto& match : matched){
-        points.insert(match.second.begin(), match.second.end());
+    for(auto& f : found){
+        points.insert(f.second.begin(), f.second.end());
     }
 
     int max_dist = 0;
-    for(auto& v0 : distances){
-        for(auto& v1 : distances){
-            max_dist =  std::max(max_dist, manhatten(v0,v1));
+    for(auto& d0 : dists){
+        for(auto& d1 : dists){
+            max_dist = std::max(max_dist, manhatten(d0,d1));
         }
     }
 
@@ -538,13 +186,11 @@ void main()
     auto actual_values = load_input("../src/day19/input.txt");
 
     auto test_result = process(test_values); 
-    std::cout << "part1: " << test_result.first << std::endl;
-    std::cout << "part1: " << test_result.second << std::endl;
-
     auto result = process(actual_values); 
-    std::cout << "part2: " << result.first << std::endl;
+
+    std::cout << "part1: " << test_result.first << std::endl;
+    std::cout << "part1: " << result.first << std::endl;
+
+    std::cout << "part2: " << test_result.second << std::endl;
     std::cout << "part2: " << result.second << std::endl;
 }
-
-
-#endif
